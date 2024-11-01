dc4c575edb663bde12fa7979348fdffcf9cdcd51
JosePaulo1990 <josepaulo.silvalima@gmail.com>
2024-11-01 11:24:57 -0300

    Aplicando View Binding. V 1.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.pdm1_aula06_exemplo1_viewbinding"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.pdm1_aula06_exemplo1_viewbinding"
--- app/src/main/java/com/example/pdm1_aula06_exemplo1_viewbinding/MainActivity.kt
+++ app/src/main/java/com/example/pdm1_aula06_exemplo1_viewbinding/MainActivity.kt
@@ -5,9 +5,14 @@ import androidx.activity.ComponentActivity
 import com.example.pdm1_aula06_exemplo1_viewbinding.databinding.ActivityMainBinding
 
 class MainActivity : ComponentActivity() {
-    private lateinit var binding: ActivityMainBinding
+    //private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
+        binding.button.setOnClickListener {
+            binding.textView.text = "Olá, View Binding!"
+        }
     }
 }
